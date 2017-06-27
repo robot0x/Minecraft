a94f3ddbbf579f73ca008694c0f8781f86ee85c1
Zhangbincheng0 <1656704949@qq.com>
2017-06-27 20:47:05 +0800

    Minecraft

---
+++ Assets/Scripts/BlockManager.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using Uniblocks;
+
+public class BlockManager : MonoBehaviour
+{
+    public int range = 10;
+
+    private ushort blockId = 0;
+    private Transform selectBlockEffect;
+
+    void Start()
+    {
+        selectBlockEffect = GameObject.Find("selected block graphics").transform;
+        selectBlockEffect.gameObject.SetActive(false);
+    }
+
+    void Update()
+    {
+        SelectBolckId();
+
+        // origin direction range ingoreTransparent 
+        VoxelInfo info = Engine.VoxelRaycast(Camera.main.transform.position, Camera.main.transform.forward, range, false);
+        if (info != null)
+        {
+            // 左键销毁
+            if (Input.GetMouseButtonDown(0))
+            {
+                Voxel.DestroyBlock(info);
+            }
+            // 中键摆放
+            if (Input.GetMouseButtonDown(1))
+            {
+                VoxelInfo newInfo = new VoxelInfo(info.adjacentIndex, info.chunk);
+                Voxel.PlaceBlock(newInfo, blockId);
+            }
+        }
+
+        UpdateSelectBolckEffect(info);
+    }
+
+    void SelectBolckId()
+    {
+        for (ushort i = 0; i < 10; i++)
+        {
+            if (Input.GetKeyDown(i.ToString()))
+            {
+                blockId = i;
+            }
+        }
+    }
+
+    void UpdateSelectBolckEffect(VoxelInfo info)
+    {
+        if (info != null)
+        {
+            selectBlockEffect.gameObject.SetActive(true);
+            selectBlockEffect.position = info.chunk.VoxelIndexToPosition(info.index);
+        }
+        else
+        {
+            selectBlockEffect.gameObject.SetActive(false);
+        }
+    }
+}
---
+++ Assets/Scripts/MapManager.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using Uniblocks;
+
+public class MapManager : MonoBehaviour
+{
+    private Transform playerTranas;
+    // private Vector3 lastPos;
+    private Index lastIndex;
+
+    void Start()
+    {
+        playerTranas = GameObject.Find("Player").transform;
+        InvokeRepeating("InitMap", 1.0f, 0.03f); // 防止进入场景卡死
+    }
+
+    void InitMap()
+    {
+        if (Engine.Initialized == false || ChunkManager.Initialized == false)
+        {
+            return;
+        }
+
+        /*Vector3 currentPos = playerTranas.position;
+        if (lastPos != currentPos)
+        {
+            ChunkManager.SpawnChunks(playerTranas.position);
+            lastPos = currentPos;
+        }*/
+
+        Index currentIdex = Engine.PositionToChunkIndex(playerTranas.position);
+        if (lastIndex != currentIdex)
+        {
+            ChunkManager.SpawnChunks(playerTranas.position);
+            lastIndex = currentIdex;
+        }
+    }
+}
